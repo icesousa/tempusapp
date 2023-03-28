a6aa5908c440b9393483df975e131dbde7b68507
icesousa <ice-l337elite@hotmail.com>
2023-03-28 17:51:42 -0300

    better

--- lib/repository.dart
+++ lib/repository.dart
@@ -1,32 +1,27 @@
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'conteudo.dart';
+
 class ConteudoRepository {
+  late Box _conteudosBox;
 
-     late Box _conteudosBox;
+  ConteudoRepository() {
+    initRepo();
+  }
 
-ConteudoRepository(){
-initRepo();
-}
-
-
-void initRepo() async{
-await Hive.initFlutter();
-_conteudosBox = await abrirCaixa();
-
-}
-
-
+  void initRepo() async {
+    await Hive.initFlutter();
+    _conteudosBox = await abrirCaixa();
+  }
 
   Box get conteudosBox => _conteudosBox;
 
   Future<Box> abrirCaixa() async {
-    
     return await Hive.openBox('conteudos');
   }
 
   Future<void> adicionarConteudo(Conteudo conteudo) async {
-    var box = await abrirCaixa();
+    var box = await Hive.openBox('conteudos');
     await box.put(conteudo.id, conteudo.tomap());
     print(box.get(conteudo.id));
   }
